868648f6bfb98714fcf9db3ce1f769c0073c6a0d
unknown <dhdbbdhd161@gmail.com>
2025-04-11 18:07:15 +0800

    prompts for number of questions and validity of input checker

--- quiz_creator_program_assignment_9.py
+++ quiz_creator_program_assignment_9.py
@@ -3,6 +3,16 @@ def main():
     print("Welcome to Quiz Creator Program!")
 
 #ask user how many questions in will be inputted
+while True:
+    try:
+        number_of_questions = int(input("Enter how many questions to input: "))
+        if number_of_questions > 0: 
+            break
+        else:
+            print("Enter a valid number greater than 0.")
+    except ValueError:
+        print("Invalid number input.")
+        
 #create a function that will ask question 
 #create function that will ask for the right answer
 #start loop, prompting to ask question and the right answer until the prompted number of questions

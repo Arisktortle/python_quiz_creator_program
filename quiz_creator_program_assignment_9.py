cf72a7e3384008f08814d6d1b2c8227d5ff16e7d
unknown <dhdbbdhd161@gmail.com>
2025-04-11 14:22:51 +0800

    pseudocode of quiz creator

---
+++ quiz_creator_program_assignment_9.py
@@ -0,0 +1,6 @@
+#welcome menu for quiz
+#ask user how many questions in will be inputted
+#create a function that will ask question 
+#create function that will ask for the right answer
+#start loop, prompting to ask question and the right answer until the prompted number of questions
+#export the questions into a file
